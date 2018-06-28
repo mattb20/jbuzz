624489a7af7c065a8c98d1a88fe7b5d4ee17203c
mattb20 <matt_burgoyne@live.co.uk>
2018-06-28 16:50:36 +0100

    second failing test written, method currently returns string instead of int

--- src/fizzbuzz/JBuzzTest.java
+++ src/fizzbuzz/JBuzzTest.java
@@ -4,11 +4,16 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class JBuzzTest {
+    JBuzz fizzbuzz = new JBuzz();
+
     @Test
     public void fizz3() {
-        JBuzz fizzbuzz = new JBuzz();
         String fizz_3 = fizzbuzz.play(3);
         assertEquals("fizz", fizz_3);
+    }
+    public void fizz7() {
+        int fizz_7 = fizzbuzz.play(7);
+        assertEquals(7, fizz_7);
     }
 
 }
