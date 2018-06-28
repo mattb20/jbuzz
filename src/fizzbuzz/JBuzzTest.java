d535255969331ac1d22e6de3ec1a894f1d91fe0e
mattb20 <matt_burgoyne@live.co.uk>
2018-06-28 18:18:51 +0100

    changed to more descriptive variable names

--- src/fizzbuzz/JBuzzTest.java
+++ src/fizzbuzz/JBuzzTest.java
@@ -7,13 +7,17 @@ public class JBuzzTest {
     JBuzz fizzbuzz = new JBuzz();
 
     @Test
-    public void fizz3() {
+    public void will_fizz3() {
         String fizz_3 = fizzbuzz.play(3);
         assertEquals("fizz", fizz_3);
     }
-    public void fizz7() {
-        String fizz_7 = fizzbuzz.play(7);
-        assertEquals(7, fizz_7);
+    public void will_return7() {
+        String number_7 = fizzbuzz.play(7);
+        assertEquals(7, number_7);
+    }
+    public void will_fizz_buzz15() {
+        String fizz_buzz15 = fizzbuzz.play(15);
+        assertEquals("fizzbuzz", fizz_buzz15);
     }
 
 }
