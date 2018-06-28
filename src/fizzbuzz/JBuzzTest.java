90870d101339e2e6304f45d22c8ec70fd9fa93fa
mattb20 <matt_burgoyne@live.co.uk>
2018-06-28 09:18:09 +0100

    import necessary files for junit5, type of fizzbuzz now matches correct class

--- src/fizzbuzz/JBuzzTest.java
+++ src/fizzbuzz/JBuzzTest.java
@@ -1,7 +1,14 @@
 package fizzbuzz;
 
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class JBuzzTest {
+    @Test
+    public void fizz3() {
+        JBuzz fizzbuzz = new JBuzz();
+        String fizz_3 = fizzbuzz.play(3);
+        assertEquals("fizz", fizz_3);
+    }
 
 }
