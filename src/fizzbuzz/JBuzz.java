ebced3bd429c20c79fe8328ed430e4ec9cc96e6c
mattb20 <matt_burgoyne@live.co.uk>
2018-07-02 07:04:55 +0100

    reordered logic in first method to account for 15 being divisible by 3 and 15

--- src/fizzbuzz/JBuzz.java
+++ src/fizzbuzz/JBuzz.java
@@ -3,8 +3,11 @@ package fizzbuzz;
 public class JBuzz {
     public static String play(int n)
     {
-        if ((n % 3) == 0) {
+        if ((n % 15) == 0) {
+            return "fizzbuzz";
+        } else if((n % 3) == 0) {
             return "fizz";
+
         } else {
             return return_integer(n);
         }
@@ -15,14 +18,15 @@ public class JBuzz {
         return Integer.toString(n);
     }
 
-    public static String[] fizzbuzz(String numbers[]) {
+    public static String[] jbuzz(String numbers[]) {
         for (int i = 0; i < numbers.length; i++) {
-            if ((Integer.parseInt(numbers[i]) % 3) == 0) {
-                numbers[i] = "fizz";
+            if ((Integer.parseInt(numbers[i]) % 15) == 0) {
+                System.out.println(numbers[i]);
+                numbers[i] = "fizzbuzz";
             } else if(Integer.parseInt(numbers[i]) % 5 == 0){
                 numbers[i] = "buzz";
-            } else if (Integer.parseInt(numbers[i]) % 15 == 0) {
-                numbers[i] = "fizzbuzz";
+            } else if (Integer.parseInt(numbers[i]) % 3 == 0) {
+                numbers[i] = "fizz";
             }
             return numbers;
         }
--- src/fizzbuzz/JBuzzTest.java
+++ src/fizzbuzz/JBuzzTest.java
@@ -27,9 +27,9 @@ public class JBuzzTest {
     public void will_return_correct_array() {
         String numbers[] = new String[]{"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15"};
         JBuzz javabuzz = new JBuzz();
-        String game[] = javabuzz.fizzbuzz(numbers);
+        String game[] = javabuzz.jbuzz(numbers);
         String expected[] = new String[]{"1","2","fizz","4","buzz","fizz","7","8","fizz", "buzz", "11", "fizz", "13", "14", "fizzbuzz"};
-        assertArrayEquals(game, expected);
+        assertArrayEquals(expected, game);
     }
 
 }
