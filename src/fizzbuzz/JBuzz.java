bb2f0573cc94c1acbbb8d7a12baf540cce3eeaf4
mattb20 <matt_burgoyne@live.co.uk>
2018-07-03 06:31:24 +0100

    fixed position of return statement, test now passing

--- src/fizzbuzz/JBuzz.java
+++ src/fizzbuzz/JBuzz.java
@@ -1,6 +1,9 @@
 package fizzbuzz;
 
 public class JBuzz {
+    public static void main(String[] args){
+
+    }
     public static String play(int n)
     {
         if ((n % 15) == 0) {
@@ -21,14 +24,12 @@ public class JBuzz {
     public static String[] jbuzz(String numbers[]) {
         for (int i = 0; i < numbers.length; i++) {
             if ((Integer.parseInt(numbers[i]) % 15) == 0) {
-                System.out.println(numbers[i]);
                 numbers[i] = "fizzbuzz";
             } else if(Integer.parseInt(numbers[i]) % 5 == 0){
                 numbers[i] = "buzz";
             } else if (Integer.parseInt(numbers[i]) % 3 == 0) {
                 numbers[i] = "fizz";
             }
-            return numbers;
         }
         return numbers;
     }
